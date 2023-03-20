0a6523a52db11d9aad91d2ff699df6b3c7f009a3
Aleksandar Gekov <alexander.gekov00@gmail.com>
2023-03-20 20:20:55 +0100

    initial template

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import { ref } from 'vue';
 import Card from './components/Card.vue';
 import Cart from './components/Cart.vue';
+import NavBar from './components/NavBar.vue';
 
 const showCart = ref(false);
 
@@ -27,20 +28,8 @@ const products = [
 
 <template>
   <div class="relative max-w-6xl mx-auto">
-    <nav class="border border-gray-300 rounded-xl rounded-t-none p-4 mb-10">
-      <div class="container flex items-center justify-between">
-        <h2 class="font-bold text-2xl w-1/3">Pinia Cart Tutorial</h2>
-        <div class="w-1/3">
-          <img class="w-12" src="../public/pinia.png" alt="">
-        </div>
-        <button @click="showCart = !showCart" class="hover:bg-gray-200 rounded-full p-2">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
-          </svg>
-        </button>
-        <Cart v-if="showCart" :products="products"/>
-      </div>
-    </nav>
+    <NavBar :products="products" :show-cart="showCart" @toggleCart="showCart = !showCart"/>
+    <Cart v-if="showCart" :products="products"/>
     <main class="flex flex-1">
       <Card v-for="product in products" :key="product.name" :product="product"/>
     </main>
